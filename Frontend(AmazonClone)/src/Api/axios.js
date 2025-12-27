025ca4042c8cba54f52c87939f3e45cf7256ccb0
geeksforweb <getanehgashaw12@gmail.com>
2025-12-27 12:02:48 +0300

    Convert submodule to a regular folder

---
+++ Frontend(AmazonClone)/src/Api/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const instance = axios.create({
+  // local instance of firebase functions
+  baseURL: "http://127.0.0.1:5001/clone-e76be/us-central1/api",
+  // deployed version of amazon server on render.com
+//   baseURL: "https://amazone-server-clone.onrender.com/",
+});
+export { instance };
